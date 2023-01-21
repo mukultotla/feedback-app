7c605c5fce338875b0a030b1eab97e236b99f7b9
Mukul <mukultotla4@gmail.com>
2023-01-22 00:53:31 +0530

    Created components and added propTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
-      <h1>Hello Mukul</h1>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} />
+      </div>
     </>
   );
 }
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
+function FeedbackList({ feedback }) {
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback yet...</p>
+    }
+    return (
+        <div className="feedback-list">
+            {
+                feedback.map((item) => (
+                    <FeedbackItem key={item.id} item={item} />
+                ))
+            }
+        </div>
+    )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.array
+}
+export default FeedbackList
